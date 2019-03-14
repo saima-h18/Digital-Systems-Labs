433e1893a8884f0366181bdc0bab4c4c1bd146db
saima-h18 <saima.haneef@mail.mcgill.ca>
2019-03-13 23:08:37 -0400

    Add files via upload

--- lab2/g14_lab2.vhd
+++ lab2/g14_lab2.vhd
@@ -10,37 +10,38 @@ port(	x			: in std_logic_vector (9 downto 0);
 		clk		: in std_logic;
 		rst		: in std_logic;
 		mac		: out std_logic_vector (21 downto 0);
-		ready		: out std_logic);
+		ready		: out std_logic;
+		counter		: out integer range 0 to 1000);
 end g14_lab2;
 
 architecture archlab2 of g14_lab2 is
 
-signal maccounter: signed(21 downto 0);	
---signal Ncounter: integer range 0 to 1000;
+signal maccounter: signed(21 downto 0);
+signal count: integer range 0 to 1000 := 0;
 
 	begin
+
 		process(clk,rst)
-		begin
+		begin	
+		
 		if rising_edge(clk) then
 			if rst = '1' then 
-				maccounter <= (to_signed(0, 22));
+				maccounter <= (to_signed(0,22));
 				ready <= '0';
+				count <= 0;
 			else
-				--Ncounter <= to_integer(to_unsigned(N, 10));
-				for_loop: for i in 1 to 1000 loop
-					maccounter <= maccounter + (signed(x) * signed(y));
-				end loop for_loop;
 				
-				ready <= '1';
---				if (N < std_logic_vector(to_unsigned(1000,10))) then
---					ready <= '0';
---					maccounter <= maccounter + (signed(x) * signed(y));
---				else
---					ready <= '1';
---					maccounter <= to_signed(0,22);
---				end if;				
+					maccounter <= maccounter + (signed(x) * signed(y));
+					count <= count + 1;
+					ready <= '0';
+					
+				if (count = to_integer(unsigned(N))-1) then
+					ready <= '1';
+				end if;				
 			end if;
-			mac <= std_logic_vector(maccounter);		
+	
 		end if;		
 	end process;
+	mac <= std_logic_vector(maccounter);
+			counter <= count;	
 end archlab2;
